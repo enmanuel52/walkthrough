df7f8094f7f9efefd28786367d6dc448eddb0e01
“enmanuel” <enmanueldelgado52@gmail.com>
2024-01-31 16:36:16 +0100

    publishing

--- walkthrough/build.gradle.kts
+++ walkthrough/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    `maven-publish`
 }
 
 android {
@@ -24,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -53,4 +54,22 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            groupId = "com.github.enmanuel52"
+            artifactId = "walkthrough"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+        }
+    }
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+    }
 }
