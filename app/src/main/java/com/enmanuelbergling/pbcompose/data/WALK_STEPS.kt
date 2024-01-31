990884223ae32afcf3b0d7eee878f0fc52a0d5d3
“enmanuel” <enmanueldelgado52@gmail.com>
2024-01-31 16:50:43 +0100

    making it lighter

---
+++ app/src/main/java/com/enmanuelbergling/pbcompose/data/WALK_STEPS.kt
@@ -0,0 +1,32 @@
+package com.enmanuelbergling.pbcompose.data
+
+import com.enmanuelbergling.pbcompose.R
+import com.enmanuelbergling.walkthrough.model.WalkStep
+
+val WALK_STEPS = arrayListOf(
+    WalkStep(
+        R.drawable.ic_explore,
+        R.string.explore,
+        R.string.explore_description
+    ),
+    WalkStep(
+        R.drawable.ic_booking,
+        R.string.bookings,
+        R.string.bookings_description
+    ),
+    WalkStep(
+        R.drawable.ic_wizard_payment,
+        R.string.pay,
+        R.string.pay_description
+    ),
+    WalkStep(
+        R.drawable.ic_wizard_waiting_room,
+        R.string.waiting_room,
+        R.string.waiting_room_description
+    ),
+    WalkStep(
+        R.drawable.ic_notifications,
+        R.string.notifications,
+        R.string.notifications_description
+    ),
+)
--- walkthrough/src/main/java/com/enmanuelbergling/walkthrough/model/WalkStep.kt
+++ walkthrough/src/main/java/com/enmanuelbergling/walkthrough/model/WalkStep.kt
@@ -9,31 +9,3 @@ data class WalkStep(
     @StringRes val titleResource: Int,
     @StringRes val descriptionResource: Int,
 )
-
-val WALK_STEPS = arrayListOf(
-    WalkStep(
-        R.drawable.ic_explore,
-        R.string.explore,
-        R.string.explore_description
-    ),
-    WalkStep(
-        R.drawable.ic_booking,
-        R.string.bookings,
-        R.string.bookings_description
-    ),
-    WalkStep(
-        R.drawable.ic_wizard_payment,
-        R.string.pay,
-        R.string.pay_description
-    ),
-    WalkStep(
-        R.drawable.ic_wizard_waiting_room,
-        R.string.waiting_room,
-        R.string.waiting_room_description
-    ),
-    WalkStep(
-        R.drawable.ic_notifications,
-        R.string.notifications,
-        R.string.notifications_description
-    ),
-)
