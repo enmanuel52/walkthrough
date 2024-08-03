b6cb280e05051cd84080aca43373cc0785a40ac3
“enmanuel” <enmanueldelgado52@gmail.com>
2024-08-03 01:58:04 -0400

    updating to stable version

--- walkthrough/build.gradle.kts
+++ walkthrough/build.gradle.kts
@@ -60,7 +60,7 @@ publishing {
         register<MavenPublication>("maven") {
             groupId = "com.github.enmanuel52"
             artifactId = "walkthrough"
-            version = "1.1.1-beta01"
+            version = "1.1.2"
 
             afterEvaluate {
                 from(components["release"])
