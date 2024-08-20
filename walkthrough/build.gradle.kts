55a2169ba418a8a6190edd802f4891b3dc7dcc9a
“enmanuel” <enmanueldelgado52@gmail.com>
2024-08-20 11:41:05 -0400

    updating version for new release

--- walkthrough/build.gradle.kts
+++ walkthrough/build.gradle.kts
@@ -60,7 +60,7 @@ publishing {
         register<MavenPublication>("maven") {
             groupId = "com.github.enmanuel52"
             artifactId = "walkthrough"
-            version = "1.1.4"
+            version = "1.1.5"
 
             afterEvaluate {
                 from(components["release"])
